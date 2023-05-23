34182aa3941b79d8c4e5fbb8a9f4d802560dc1d6
Hugo Frota <herbtyh@gmail.com>
2023-05-23 10:00:32 -0300

    Adicionar página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
+import { MdFlightTakeoff } from 'react-icons/md'
+
 import api from '../../services/api'
+import './style.css'
 
 const Home = () => {
   const [trips, setTrips] = useState([]);
@@ -15,7 +18,27 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? "Disponivel" : "Indisponivel"}</span>
+
+            <button
+            type='button'
+            onClick={() => {}}
+            >
+              <div>
+                <MdFlightTakeoff size={16} color='#fff'/>
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
   )
 }
 
