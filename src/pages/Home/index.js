f29e664ec385efa9b87abb19cc88856f4529f9c2
Hugo Frota <herbtyh@gmail.com>
2023-05-22 21:35:53 -0300

    Adicionar api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import api from '../../services/api'
 
 const Home = () => {
+  const [trips, setTrips] = useState([]);
+  
+  useEffect(() => {
+    async function loadApi(){
+      const response = await api.get('trips');
+      setTrips(response.data);
+      console.log(response.data);
+    }
+
+    loadApi();
+  }, [])
+
   return (
     <div>Home</div>
   )
