da1f59a8429615015972bf6a45f8a90079b97f06
Hugo Frota <herbtyh@gmail.com>
2023-05-27 14:23:15 -0300

    Adicionar ação de remover item da lista

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdDelete } from 'react-icons/md';
 
 import './style.css';
 
 const Reservas = () => {
+  const dispatch = useDispatch()
   const reserves = useSelector(state => state.reserve)
+
+  function handleRemove(id){
+    dispatch({
+      type: 'REMOVE_RESERVE',
+      id,
+    })
+  }
 
   return (
     <div>
@@ -16,7 +24,7 @@ const Reservas = () => {
           <img src={reserve.image} alt={reserve.title} />
           <strong>{reserve.title}</strong>
           <span>Quantidade: {reserve.amount}</span>
-          <button type='button' onClick={() => {}}>
+          <button type='button' onClick={() => handleRemove(reserve.id)}>
             <MdDelete size={20} color='#191919'/>
           </button>
         </div>
